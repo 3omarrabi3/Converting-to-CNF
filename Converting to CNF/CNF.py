eb7302bb0a488e54bfe827aea382e46898aaa3f5
AmmarAyman1411 <93484721+AmmarAyman1411@users.noreply.github.com>
2024-03-29 14:10:52 +0200

    CNF Update 3.0

--- Converting to CNF/CNF.py
+++ Converting to CNF/CNF.py
@@ -122,8 +122,7 @@ def replace_variables(match):
     unique_var_map = {}  # Store the unique variable assigned to each original variable
     for var in variables:
         if '(' in var:  # Check if the variable contains parentheses, indicating it's a function call
-            function_name, argument = var.split('(',
-                                                maxsplit=1)  # Split the function call into function name and argument
+            function_name, argument = var.split('(',  maxsplit=1)  # Split the function call into function name and argument
             inner_variables = re.findall(r'\b\w+\b', argument)  # Find all simple variables in the function argument
             replaced_argument = ''
             for inner_var in inner_variables:
@@ -136,14 +135,13 @@ def replace_variables(match):
                             break
                         replaced_var += letter
                     replaced_argument += replaced_var
-                    unique_var_map[inner_var] = replaced_argument[
-                                                :1]  # Store the unique variable for this original variable
+                    unique_var_map[inner_var] = replaced_argument[ :1]  # Store the unique variable for this original variable
                 replaced_argument += ', '  # Add comma and space for multiple variables
             replaced_argument = replaced_argument.rstrip(', ')  # Remove the trailing comma and space
             replaced_var = f"{function_name}({replaced_argument}"  # Reconstruct the function call
         else:
             replaced_var = unique_var  # Replace simple variables with the unique letter
-        replaced_statement = replaced_statement.replace(var, replaced_var, 1)  # Replace only the first occurrence
+        replaced_statement = replaced_statement.replace(var[:5], replaced_var[:5], 1)  # Replace only the first occurrence
     return replaced_statement
 
 
@@ -163,17 +161,17 @@ def converting_to_CNF(formula):
     return result
 
 
-formula1 = ('For all x Exist y (p(x) -> q(y))')
-formula2 = ('For all x (p(x) <-> q(x))')
-formula3 = ('For all x ~(p(x) | q(x))')
-formula4 = ('For all x ~~(p(x) | q(x))')
-formula5 = ('For all x ~~(p(x) | q(x)) & For all y ~(p(y) | q(y))')
-formula6 = ('For all x (p(x) | q(x)) & Exist y (p(x) | q(y))')
-formula7 = ('For all x (~p(x) | q(x))')
-formula8 = ('For all x Exist y ~(p(x) | q(y))')
-formula9 = ('For all x ~(p(x) & q(x))')
-formula10 = ('For all x (~p(x) | ~q(x)) & Exist y ((z(x) <-> c(y)) & For all z (p(x) -> r(z))')
-formula11 = ('For all x (~p(x) | ~q(x)) & Exist y ((z(x) | ~c(y)) & For all z (p(x) -> r(z)) & Exist g (p(x) -> r(g))')
+formula1 = ('For all x Exist y (P(x) -> Q(y))')
+formula2 = ('For all x (P(x) <-> Q(x))')
+formula3 = ('For all x ~(P(x) | Q(x))')
+formula4 = ('For all x ~~(P(x) | Q(x))')
+formula5 = ('For all x ~~(P(x) | Q(x)) & For all y ~(P(y) | Q(y))')
+formula6 = ('For all x (P(x) | Q(x)) & Exist y (P(x) | Q(y))')
+formula7 = ('For all x (~P(x) | Q(x))')
+formula8 = ('For all x Exist y ~(P(x) | Q(y))')
+formula9 = ('For all x ~(P(x) & Q(x))')
+formula10 = ('For all x (~P(x) | ~Q(x)) & Exist y ((Z(x) <-> C(y)) & For all z (P(x) -> R(z))')
+formula11 = ('For all x (~P(x) | ~Q(x)) & Exist y ((Z(x) | ~C(y)) & For all z (P(x) -> R(z)) & Exist g (P(x) -> R(g))')
 
 print("Test Cases To Convert to CNF")
 print("\n------------ Test Cases For Formula 1 ------------")
